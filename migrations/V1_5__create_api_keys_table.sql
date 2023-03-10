5c1a0729b0adc56437a61b5eabbc93f2ce4ed567
Boris Claus Verhaaff <boris@c4rex.dev>
2023-03-10 18:09:42 +0100

    updated schema

--- migrations/V1_5__create_api_keys_table.sql
+++ migrations/V1_5__create_api_keys_table.sql
@@ -3,10 +3,10 @@ DROP TABLE api_keys;
 CREATE TABLE api_keys (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   user_id INT UNSIGNED NOT NULL,
-  key VARCHAR(64) NOT NULL,
-  permissions JSON NOT NULL,
+  `key` VARCHAR(64) NOT NULL,
+  permissions LONGTEXT NOT NULL,
   expiration_date DATE NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY (key),
+  UNIQUE KEY (`key`),
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
