32151465f3497542906756bdbc1b0a1526cf11ee
Boris Claus Verhaaff <boris@c4rex.dev>
2023-03-13 13:50:31 +0100

    updated flyway

--- migrations/V1_8__create_email_verification.sql
+++ migrations/V1_8__create_email_verification.sql
@@ -1,12 +1,8 @@
-CREATE TABLE subscriptions (
+CREATE TABLE email_verification (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL,
-  stripe_customer_id VARCHAR(255) NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
-  status ENUM('active', 'canceled') NOT NULL DEFAULT 'active',
-  stripe_subscription_id VARCHAR(255) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-    ON DELETE CASCADE
+  verification_code VARCHAR(64) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 )
 ENGINE=InnoDB;
