8d9bc2a1e8710b1b47a839ff66a4809329fd5277
Boris Claus Verhaaff <boris@c4rex.dev>
2023-03-13 12:20:06 +0100

    updated

--- backend/test/textmod-email/index.test.ts
+++ backend/test/textmod-email/index.test.ts
@@ -1,23 +1,32 @@
-import tape from "tape";
-
+import test from 'tape';
 import { EmailAddress, EmailClient, EmailMessage, EmailRecipients, PlainTextEmailContent } from '@azure/communication-email';
 import { EmailSendConnectionManager } from '../../src/textmod-email/index';
 
-// Create an instance of the email send connection manager
-const emailManager = new EmailSendConnectionManager(process.env.AZ_COM_EMAIL_SERVICE_CONNECTION_STRING as string);
+test('Test sending email', async (t) => {
+  // Create an instance of the email send connection manager
+  const emailManager = new EmailSendConnectionManager(process.env.AZ_COM_EMAIL_SERVICE_CONNECTION_STRING as string);
 
-// Define the email message
-const email: EmailMessage = {
-  senderAddress: 'no-reply@textmod.xyz',
-  replyTo: [{ address: 'no-reply@textmod.xyz' } as EmailAddress],
-  content: {
-    subject: "Test subject",
-    plainText: "Test plainText message",
-    html: '<html><body>Hello html text</body></html>'
-  } as PlainTextEmailContent,
-  recipients: { to: [{ address: 'boris@c4rex.dev' } as EmailAddress]} as EmailRecipients
-};
+  // Define the email message
+  const email: EmailMessage = {
+    senderAddress: 'no-reply@textmod.xyz',
+    replyTo: [{ address: 'no-reply@textmod.xyz' } as EmailAddress],
+    content: {
+      subject: "Test subject",
+      plainText: "Test plainText message",
+      html: '<html><body>Hello html text</body></html>'
+    } as PlainTextEmailContent,
+    recipients: { to: [{ address: 'boris@c4rex.dev' } as EmailAddress]} as EmailRecipients
+  };
 
-// Send the email
-emailManager.sendEmail(email)
-  .catch(error => console.error('Error sending email:', error));
+  // Send the email
+  try {
+    await emailManager.sendEmail(email);
+    t.pass('Email sent successfully');
+    process.exitCode = 0; // Set exit code to 0 on success
+  } catch (error) {
+    t.fail(`Error sending email: ${error}`);
+    process.exitCode = 1; // Set exit code to 1 on failure
+  }
+
+  t.end();
+});
